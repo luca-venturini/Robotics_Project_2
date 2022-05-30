51674a1fbf11ccc94da394b42ba85e74c8133a5f
matteo-fabris <10623973@polimi.it>
2022-05-30 18:18:08 +0200

    path added

---
+++ src/src/show_path.cpp
@@ -0,0 +1,37 @@
+#include "ros/ros.h"
+#include "nav_msgs/Path.h"
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include "geometry_msgs/PoseStamped.h"
+
+class ShowPath
+{
+public:
+  ShowPath() {
+    pub = n.advertise<nav_msgs::Path>("path", 1000);
+    sub = n.subscribe("/amcl_pose", 1000, &ShowPath::callback, this);
+  }
+
+  void callback(const geometry_msgs::PoseWithCovarianceStamped& msg){
+    path.header.stamp = ros::Time::now();
+    path.header.frame_id = "map";
+    geometry_msgs::PoseStamped pose_stamped;
+    pose_stamped.header = msg.header;
+    pose_stamped.pose = msg.pose.pose;
+    path.poses.push_back(pose_stamped);
+    pub.publish(path);
+  }
+
+private:
+  nav_msgs::Path path;
+  ros::NodeHandle n;
+  ros::Subscriber sub;
+  ros::Publisher pub;
+};
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "path_broadcaster");
+  ShowPath path_broadcaster;
+  ros::spin();
+  return 0;
+}
